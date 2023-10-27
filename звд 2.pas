3c47df068a79339b269295eef202ec4dedb7f928
kuraeshka <144653643+kuraeshka@users.noreply.github.com>
2023-10-27 10:24:39 +0300

    Add files via upload

---
+++ звд 2.pas
@@ -0,0 +1,17 @@
+﻿var
+  n:array [1..20] of integer;
+  i,g,f:integer;
+  begin
+    f:=1; 
+    for i:=1 to 20 do
+      begin
+      n[i]:=-22 +random(116);
+      if (n[i] mod 2 = 0) and (i mod 2 <>0) then g:=g+1;
+      if n[i] mod 2 = 1 then  f:=f*n[i];
+      end;
+      write(n);
+      writeln();
+      write(g);
+      writeln();
+      write(f);
+  end.
